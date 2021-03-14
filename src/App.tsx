a5c4de613d35b93c8e78a4154178d5dfa260cb15
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2021-03-14 18:03:21 -0300

    Created friends section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {FiBell, FiChevronDown, FiChevronLeft, FiChevronRight, FiColumns, FiDownload, FiHeadphones, FiHelpCircle, FiHome, FiMic, FiSearch, FiSettings } from 'react-icons/fi';
+import {FiBell, FiChevronDown, FiChevronLeft, FiChevronRight, FiColumns, FiDownload, FiHeadphones, FiHelpCircle, FiHome, FiMic, FiPlayCircle, FiSearch, FiSettings } from 'react-icons/fi';
 import { FaFilter } from 'react-icons/fa';
 
 import './styles/home.css'
@@ -7,6 +7,13 @@ function App() {
   return (
     <div className="container">
       <div className="music-section">
+        <div className="music">
+          <img src="https://i.pinimg.com/originals/5d/97/1f/5d971f49f2ccbda4f9a8ee2bc98f4a16.jpg" alt="music cover"/>
+          <div className="about-music">
+            <p className="music-title">Riptide</p>
+            <p className="artist-name">Vance joy</p>
+          </div>
+        </div>
       </div>
       <div className="main-section">
         <div className="left-nav">
@@ -50,7 +57,7 @@ function App() {
                 <li><button><FiBell size="20" color="#FFF" /></button></li>
               </ul>
               <div className="profile">
-                <button><img src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png" alt="Profile" /></button>
+                <button><img src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="Profile" /></button>
                 <button><FiChevronDown size="24" color="#FFF" /></button>
               </div>
             </div>
@@ -92,8 +99,80 @@ function App() {
             </div>
             <div className="right-content">
               <div className="friends-section">
-                <div className="friends-box">
-
+                <p className="friends-title">Friends Activity</p>
+                <div className="friends">
+                  <div className="friend">
+                    <img src="https://images.unsplash.com/photo-1534564533601-4d3e3d9fd229?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="friend"/>
+                    <div className="friend-listening">
+                      <p className="friend-name">Willian</p>
+                      <p className="friend-music">Atlantis</p>
+                      <p className="friend-artist">Seafret</p>
+                      <div className="play-friend-song">
+                        <FiPlayCircle size="20" color="#FFF"/>
+                        <span>Relaxing</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="friend">
+                    <img src="https://images.unsplash.com/photo-1526510747491-58f928ec870f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="friend"/>
+                    <div className="friend-listening">
+                      <p className="friend-name">Tracie</p>
+                      <p className="friend-music">HIGHEST IN THE ROOM</p>
+                      <p className="friend-artist">Travis Scott</p>
+                      <div className="play-friend-song">
+                        <FiPlayCircle size="20" color="#FFF"/>
+                        <span>Trapzin</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="friend">
+                    <img src="https://images.unsplash.com/photo-1506634572416-48cdfe530110?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=632&q=80" alt="friend"/>
+                    <div className="friend-listening">
+                      <p className="friend-name">Mike</p>
+                      <p className="friend-music">Heart Afire</p>
+                      <p className="friend-artist">Defqwop</p>
+                      <div className="play-friend-song">
+                        <FiPlayCircle size="20" color="#FFF"/>
+                        <span>Animadão</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="friend">
+                    <img src="https://images.unsplash.com/photo-1517374096549-0b818e3de484?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" alt="friend"/>
+                    <div className="friend-listening">
+                      <p className="friend-name">Celine</p>
+                      <p className="friend-music">Crazy</p>
+                      <p className="friend-artist">Aerosmith</p>
+                      <div className="play-friend-song">
+                        <FiPlayCircle size="20" color="#FFF"/>
+                        <span>Rock oldschool</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="friend">
+                    <img src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8cG9ydHJhaXR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="friend"/>
+                    <div className="friend-listening">
+                      <p className="friend-name">Alice</p>
+                      <p className="friend-music">Aurora</p>
+                      <p className="friend-artist">Runaway</p>
+                      <div className="play-friend-song">
+                        <FiPlayCircle size="24" color="#FFF"/>
+                        <span>Fazendo nada</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="friend">
+                    <img src="https://images.unsplash.com/photo-1521119989659-a83eee488004?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80" alt="friend"/>
+                    <div className="friend-listening">
+                      <p className="friend-name">Ryan</p>
+                      <p className="friend-music">Circles</p>
+                      <p className="friend-artist">Post Malone</p>
+                      <div className="play-friend-song">
+                        <FiPlayCircle size="24" color="#FFF"/>
+                        <span>Caboaideia</span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
