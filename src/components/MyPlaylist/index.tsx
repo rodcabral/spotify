99d653152aeed92ea26a229575234ca6f1c5c385
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2021-03-15 23:12:42 -0300

    Refactoring

--- src/components/MyPlaylist/index.tsx
+++ src/components/MyPlaylist/index.tsx
@@ -2,6 +2,26 @@ import { FiChevronLeft, FiChevronRight, FiPlayCircle } from 'react-icons/fi'
 import './styles.css'
 
 export default function MyPlaylist() {
+
+
+    const playlists = [
+        {
+            image: 'https://static.highsnobiety.com/thumbor/QEQoC9kEVuoS3jwz8n_gJ632IUk=/1600x1067/static.highsnobiety.com/wp-content/uploads/2020/02/25170803/lofi-girl.jpg',
+            name: 'Lo-fi',
+        },
+        {
+            image: 'https://i.pinimg.com/564x/05/5f/b9/055fb9aa453dfc62e84af468b31c0fb7.jpg',
+            name: 'Coffe Time',
+        }, {
+            image: 'https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=8',
+            name: 'Coding',
+        }, {
+            image: 'https://cutewallpaper.org/21/trippy-art-wallpaper/iPhone-Wallpaper-Psychedelic-art,-Art,-Cg-artwork-.jpg',
+            name: 'Random',
+        },
+    ]
+
+
     return (
         <div className="playlist-section">
             <div className="playlist-header">
@@ -16,27 +36,19 @@ export default function MyPlaylist() {
                 </div>
             </div>
             <div className="playlist-container">
-                <div className="playlist-box">
-                    <div className="image-container">
-                        <img src="https://static.highsnobiety.com/thumbor/QEQoC9kEVuoS3jwz8n_gJ632IUk=/1600x1067/static.highsnobiety.com/wp-content/uploads/2020/02/25170803/lofi-girl.jpg" alt="playlist" />
-                        <div className="play-playlist">
-                            <button><FiPlayCircle size="45" color="#FFF"/></button>
+                {
+                    playlists.map(playlist => (
+                        <div className="playlist-box">
+                            <div className="image-container">
+                                <img src={playlist.image} alt="playlist" />
+                                <div className="play-playlist">
+                                    <button><FiPlayCircle size="45" color="#FFF" /></button>
+                                </div>
+                            </div>
+                            <span>{playlist.name}</span>
                         </div>
-                    </div>
-                    <span>Lo-fi</span>
-                </div>
-                <div className="playlist-box">
-                    <img src="https://i.pinimg.com/564x/05/5f/b9/055fb9aa453dfc62e84af468b31c0fb7.jpg" alt="playlist" />
-                    <span>Coffe time</span>
-                </div>
-                <div className="playlist-box">
-                    <img src="https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80" alt="playlist" />
-                    <span>Coding</span>
-                </div>
-                <div className="playlist-box">
-                    <img src="https://cutewallpaper.org/21/trippy-art-wallpaper/iPhone-Wallpaper-Psychedelic-art,-Art,-Cg-artwork-.jpg" alt="playlist" />
-                    <span>Random</span>
-                </div>
+                    ))
+                }
             </div>
         </div>
     )
--- src/components/Recommended/index.tsx
+++ src/components/Recommended/index.tsx
@@ -1,7 +1,26 @@
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+import { FiChevronLeft, FiChevronRight, FiPlayCircle } from 'react-icons/fi'
 import './styles.css'
 
 export default function Recommended() {
+
+    const playlists = [
+        {
+            image: 'https://wallpapercave.com/wp/Z0kmvgB.jpg',
+            name: 'Rain Day',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1487956382158-bb926046304a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
+            name: 'Walking',
+        }, {
+            image: 'https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            name: 'Clean Home',
+        }, {
+            image: 'https://images.unsplash.com/photo-1505147704403-c2caf85db293?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1048&q=80',
+            name: 'With Friends',
+        },
+    ]
+
+
     return (
         <div className="recomendded-section">
             <div className="recomendded-header">
@@ -16,22 +35,19 @@ export default function Recommended() {
                 </div>
             </div>
             <div className="recomendded-container">
-                <div className="recomendded-box">
-                    <img src="https://wallpapercave.com/wp/Z0kmvgB.jpg" alt="recomendded" />
-                    <span>Rain Day</span>
-                </div>
-                <div className="recomendded-box">
-                    <img src="https://images.unsplash.com/photo-1487956382158-bb926046304a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" alt="recomendded" />
-                    <span>Walking</span>
-                </div>
-                <div className="recomendded-box">
-                    <img src="https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="recomendded" />
-                    <span>Clean home</span>
-                </div>
-                <div className="recomendded-box">
-                    <img src="https://images.unsplash.com/photo-1505147704403-c2caf85db293?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1048&q=80" alt="recomendded" />
-                    <span>Best of J Cole</span>
-                </div>
+                {
+                    playlists.map(playlist => (
+                        <div className="playlist-box">
+                            <div className="image-container">
+                                <img src={playlist.image} alt="playlist" />
+                                <div className="play-playlist">
+                                    <button><FiPlayCircle size="45" color="#FFF" /></button>
+                                </div>
+                            </div>
+                            <span>{playlist.name}</span>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     )
